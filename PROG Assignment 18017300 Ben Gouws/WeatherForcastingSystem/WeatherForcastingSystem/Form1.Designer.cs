f6a4a7c0b2a717238c3fef08c20dca78b2984358
KingEzekiel707 <49250276+KingEzekiel707@users.noreply.github.com>
2019-04-09 12:15:51 +0200

    the edited program

---
+++ PROG Assignment 18017300 Ben Gouws/WeatherForcastingSystem/WeatherForcastingSystem/Form1.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace WeatherForcastingSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnView = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.btnEData = new System.Windows.Forms.Button();
+            this.dTime = new System.Windows.Forms.DateTimePicker();
+            this.nudMin = new System.Windows.Forms.NumericUpDown();
+            this.nudMax = new System.Windows.Forms.NumericUpDown();
+            this.nudWind = new System.Windows.Forms.NumericUpDown();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.dStart = new System.Windows.Forms.DateTimePicker();
+            this.dEnd = new System.Windows.Forms.DateTimePicker();
+            this.dgv1 = new System.Windows.Forms.DataGridView();
+            this.label10 = new System.Windows.Forms.Label();
+            this.nudHumidity = new System.Windows.Forms.NumericUpDown();
+            this.label11 = new System.Windows.Forms.Label();
+            this.nudPrec = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMax)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudWind)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudHumidity)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPrec)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(90, 55);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(116, 39);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add Forcast";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnView
+            // 
+            this.btnView.Location = new System.Drawing.Point(90, 251);
+            this.btnView.Name = "btnView";
+            this.btnView.Size = new System.Drawing.Size(116, 42);
+            this.btnView.TabIndex = 1;
+            this.btnView.Text = "View Forcast";
+            this.btnView.UseVisualStyleBackColor = true;
+            this.btnView.Click += new System.EventHandler(this.btnView_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(332, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Weather Forcasting System";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(595, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "City";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(589, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Date";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(508, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Minimum Temperature";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(505, 151);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Maximum Temperature";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(541, 174);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Wind speed /d";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(635, 47);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(557, 55);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(62, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Previos City";
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(635, 73);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(100, 20);
+            this.txtCity.TabIndex = 10;
+            // 
+            // btnEData
+            // 
+            this.btnEData.Location = new System.Drawing.Point(793, 174);
+            this.btnEData.Name = "btnEData";
+            this.btnEData.Size = new System.Drawing.Size(75, 23);
+            this.btnEData.TabIndex = 15;
+            this.btnEData.Text = "Enter Data";
+            this.btnEData.UseVisualStyleBackColor = true;
+            this.btnEData.Click += new System.EventHandler(this.btnEData_Click);
+            // 
+            // dTime
+            // 
+            this.dTime.Location = new System.Drawing.Point(635, 96);
+            this.dTime.Name = "dTime";
+            this.dTime.Size = new System.Drawing.Size(200, 20);
+            this.dTime.TabIndex = 16;
+            // 
+            // nudMin
+            // 
+            this.nudMin.Location = new System.Drawing.Point(636, 120);
+            this.nudMin.Name = "nudMin";
+            this.nudMin.Size = new System.Drawing.Size(120, 20);
+            this.nudMin.TabIndex = 17;
+            // 
+            // nudMax
+            // 
+            this.nudMax.Location = new System.Drawing.Point(635, 144);
+            this.nudMax.Name = "nudMax";
+            this.nudMax.Size = new System.Drawing.Size(120, 20);
+            this.nudMax.TabIndex = 18;
+            // 
+            // nudWind
+            // 
+            this.nudWind.Location = new System.Drawing.Point(635, 167);
+            this.nudWind.Name = "nudWind";
+            this.nudWind.Size = new System.Drawing.Size(120, 20);
+            this.nudWind.TabIndex = 19;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(530, 266);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(55, 13);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "Start Date";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(530, 298);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(52, 13);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "End Date";
+            // 
+            // dStart
+            // 
+            this.dStart.Location = new System.Drawing.Point(592, 266);
+            this.dStart.Name = "dStart";
+            this.dStart.Size = new System.Drawing.Size(200, 20);
+            this.dStart.TabIndex = 22;
+            // 
+            // dEnd
+            // 
+            this.dEnd.Location = new System.Drawing.Point(592, 298);
+            this.dEnd.Name = "dEnd";
+            this.dEnd.Size = new System.Drawing.Size(200, 20);
+            this.dEnd.TabIndex = 23;
+            // 
+            // dgv1
+            // 
+            this.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv1.Location = new System.Drawing.Point(119, 324);
+            this.dgv1.Name = "dgv1";
+            this.dgv1.Size = new System.Drawing.Size(673, 188);
+            this.dgv1.TabIndex = 24;
+            this.dgv1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(572, 196);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(47, 13);
+            this.label10.TabIndex = 25;
+            this.label10.Text = "Humidity";
+            // 
+            // nudHumidity
+            // 
+            this.nudHumidity.Location = new System.Drawing.Point(635, 188);
+            this.nudHumidity.Name = "nudHumidity";
+            this.nudHumidity.Size = new System.Drawing.Size(120, 20);
+            this.nudHumidity.TabIndex = 26;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(554, 218);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(65, 13);
+            this.label11.TabIndex = 27;
+            this.label11.Text = "Precipitation";
+            // 
+            // nudPrec
+            // 
+            this.nudPrec.Location = new System.Drawing.Point(636, 210);
+            this.nudPrec.Name = "nudPrec";
+            this.nudPrec.Size = new System.Drawing.Size(120, 20);
+            this.nudPrec.TabIndex = 28;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(891, 519);
+            this.Controls.Add(this.nudPrec);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.nudHumidity);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.dgv1);
+            this.Controls.Add(this.dEnd);
+            this.Controls.Add(this.dStart);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.nudWind);
+            this.Controls.Add(this.nudMax);
+            this.Controls.Add(this.nudMin);
+            this.Controls.Add(this.dTime);
+            this.Controls.Add(this.btnEData);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnView);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudMin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMax)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudWind)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudHumidity)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPrec)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.Button btnEData;
+        private System.Windows.Forms.DateTimePicker dTime;
+        private System.Windows.Forms.NumericUpDown nudMin;
+        private System.Windows.Forms.NumericUpDown nudMax;
+        private System.Windows.Forms.NumericUpDown nudWind;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.DateTimePicker dStart;
+        private System.Windows.Forms.DateTimePicker dEnd;
+        private System.Windows.Forms.DataGridView dgv1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.NumericUpDown nudHumidity;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.NumericUpDown nudPrec;
+    }
+}
+
---
+++ PROG Assignment 18017300 Ben Gouws/WeatherForcastingSystem/WeatherForcastingSystem/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeatherForcastingSystem
+{
+    public partial class Form1 : Form
+    {
+
+
+
+        /// <summary>
+        /// ooral waar ek ge-edit het se comments sal >> || << die simbool voor aan he
+        /// jammer ek het jou spacing bietjie rond geneek
+        /// </summary>
+        
+
+
+        // || added static before int
+        static int iMinTemp, iMaxTemp, iWindspeed , iCount, iPrecipitation;//Public veriables for task 1
+        int x, y;//The possition in the array.
+        int iCity;//This is to incrase the city array.
+        
+        
+        // || your varibles where complaining about not being static, so i made them static. but they are still varibles
+
+         string[] arrCity = new string[iCount];        //I need it to take my iCount as a uknown veriable.
+        DateTime[] arrDate = new DateTime[iCount];     //I need it to take my iCount as a uknown veriable.
+        int[] arrMinTemp = new int[iCount];            //I need it to take my iCount as a uknown veriable.
+        int[] arrMaxTemp = new int[iCount];            //I need it to take my iCount as a uknown veriable.
+        int[] arrWindSpeed = new int[iCount];         //I need it to take my iCount as a uknown veriable.
+        int[] arrPrecipitation = new int[iCount];       //I need it to take my iCount as a uknown veriable.
+        int[] arrHumidity = new int[iCount];            //I need it to take my iCount as a uknown veriable.
+
+
+
+
+        string sCity;//to capture the city name and put it in a array later.
+        DateTime dDate;//to capture the date and put it in a array later.
+
+        
+
+        private void btnEData_Click(object sender, EventArgs e)
+        {
+            x = 0;   // || i added converstion becase the value you are trying to store are decimals.
+                     // || note converting a decimal to a integer could potential cause loss of sensitive data(google that)
+                     // || solution to above problem is then store your values as decimals or reals
+           int iMinTemp = Convert.ToInt32(nudMin.Value);              //I want to take numeric up down to int.
+            int iMaxTemp = Convert.ToInt32(nudMax.Value);              //I want to take numeric up down to int.
+            int iWindSpeed = Convert.ToInt32(nudWind.Value);            //I want to take numeric up down to int.
+            int iPrecipitation = Convert.ToInt32(nudPrec.Value);        //I want to take numeric up down to int.
+            int iHumidity = Convert.ToInt32(nudHumidity.Value);         //I want to take numeric up down to int.
+
+            // || Once you declare the array, you dont have to use the datatypes infront of it again
+            arrCity[x] = txtCity.Text;         //It is not taking my x int. || you where redeclaring the array
+            arrDate[x] = dTime.MaxDate;        // it is not taking my x int.  
+            arrMinTemp[x] = iMinTemp;//insirt Minimum tempreture in array.
+            arrMaxTemp[x] = iMaxTemp;//insirt Maximum tempreture in array.
+            arrPrecipitation[x] = iPrecipitation;//insert Precipitation.
+            arrHumidity[x] = iHumidity;
+            iCount++;//icrease iCount to move to next array.
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnView_Click(object sender, EventArgs e)
+        { DateTime dtStart,dtEnd;
+            dtStart = dStart.MaxDate;
+            dtEnd = dEnd.MinDate;
+            x = 1;
+            dgv1.Rows[0].Cells[0].Value = "Date";
+            dgv1.Rows[0].Cells[1].Value = "City";
+            dgv1.Rows[0].Cells[2].Value = "Minimum Tempreture";
+            dgv1.Rows[0].Cells[3].Value = "Maximum Tempreture";
+            dgv1.Rows[0].Cells[4].Value = "Precipitation";
+            dgv1.Rows[0].Cells[5].Value = "Humidty";
+            dgv1.Rows[0].Cells[6].Value = "Wind Speed";
+            while (x < iCount)
+               
+            {
+                // || first error was your array's didnt have indexes(die x)
+                // || second error, your if didnt encapsulate both arguments( you needed to put both in one bracket)
+                if( (arrDate[x]> dStart.MaxDate) & (arrDate[x] < dtEnd.Date)  )   //I need to add the dates between start date and end date to the grid view.
+                {
+                    dgv1.Rows[x].Cells[0].Value = arrDate[x];//Add to grid view.
+                    dgv1.Rows[x].Cells[1].Value = arrCity[x];//Add to grid view.
+                    dgv1.Rows[x].Cells[2].Value = arrMinTemp[x];//Add to grid view.
+                    dgv1.Rows[x].Cells[3].Value = arrMaxTemp[x];//Add to grid view.
+                    dgv1.Rows[x].Cells[4].Value = arrPrecipitation[x];//Add to grid view.
+                    dgv1.Rows[x].Cells[5].Value = arrHumidity[x];//Add to grid view.
+                    dgv1.Rows[x].Cells[6].Value = arrWindSpeed[x];//Add to grid view.
+
+                }
+            } 
+            
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            x=0;
+            y=0;
+            iCount = 0;
+
+
+        }
+    }
+}
