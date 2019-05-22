1995250bbd114c531801212a4adf24073caa2d68
CrossbowCat <50926285+CrossbowCat@users.noreply.github.com>
2019-05-22 22:29:17 +0200

    Added comment

--- PROG Assignment 18017300 Ben Gouws/WeatherForcastingSystem/WeatherForcastingSystem/Form1.cs
+++ PROG Assignment 18017300 Ben Gouws/WeatherForcastingSystem/WeatherForcastingSystem/Form1.cs
@@ -18,6 +18,7 @@ namespace WeatherForcastingSystem
         /// <summary>
         /// ooral waar ek ge-edit het se comments sal >> || << die simbool voor aan he
         /// jammer ek het jou spacing bietjie rond geneek
+        //Fuck oFF
         /// </summary>
         
 
